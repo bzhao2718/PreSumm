1ca69c29c2978a899bef136849ed1bee27c0dadc
Jack Zhao <emailserver2718@gmail.com>
2020-12-29 00:18:04 -0600

    cleaned some duplicates

--- src/train.py
+++ src/train.py
@@ -188,3 +188,4 @@ if __name__ == '__main__':
 
 # add this arg when training with checkpoint: -train_from ../models/xsum/model_step_10.pt
 
+# python train.py  -task abs -mode train -bert_data_path ../bert_data/bert_data_xsum/xsum -dec_dropout 0.2  -model_path ../models/xsum -sep_optim true -lr_bert 0.002 -lr_dec 0.2 -save_checkpoint_steps 1 -batch_size 140 -train_steps 200000 -report_every 1 -accum_count 5 -use_bert_emb true -use_interval true -warmup_steps_bert 20000 -warmup_steps_dec 10000 -max_pos 512 -visible_gpus -1  -log_file ../logs/abs_bert_xsum
